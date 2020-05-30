63c5a2c2b26003d32b08f01d46093e1e7ba94ea3
ronen <ronen567@gmail.com>
2020-05-30 19:02:44 +0300

    fix size

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl }) => (
+const MenuItem = ({ title, imageUrl, size }) => (
 	<div
-		className="menu-item"
 		style={{
 			backgroundImage: `url(${imageUrl})`
 		}}
+		className={`${size} menu-item`}
 	>
 		<div className="content">
 			<h1 className="title">{title.toUpperCase()}</h1>
