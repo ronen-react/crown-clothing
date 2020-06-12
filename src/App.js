d03c473c17ade2bfd8f9c70449d8c94c7c73416a
ronen <ronen567@gmail.com>
2020-06-12 15:15:05 +0300

    header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
+import Header from './components/header/header.component';
 // const HatsPage = () => (
 // 	<div>
 // 		<h1>HATS PAGE</h1>
@@ -14,6 +15,7 @@ import ShopPage from './pages/shop/shop.component';
 function App() {
 	return (
 		<div>
+			<Header />
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route path="/shop" component={ShopPage} />
